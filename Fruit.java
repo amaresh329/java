8cf53a4e7b75dbaceb97d0ff6f01b0eddda45841
amaresh329 <94879341+amaresh329@users.noreply.github.com>
2021-12-15 00:49:13 +0530

    Add files via upload

---
+++ Fruit.java
@@ -0,0 +1,34 @@
+public class Fruit {
+    String name;
+    String taste;
+    int size;
+    public void eat(String n,String t){
+        n=name;
+        t=taste;
+        // comment
+
+    }
+
+    public static void main(String[] args) {
+        Apple obj1 = new Apple();
+        Orange obj2 = new Orange();
+        obj1.eat("Apple","Sweety");
+        obj2.eat("Orange","Juicy");
+    }
+}
+class Apple extends Fruit{
+    @Override
+    public void eat(String n, String t){
+        n="Apple";
+        System.out.println(n+" "+t);
+    }
+
+}
+class Orange extends Fruit{
+    @Override
+    public void eat(String n,String t){
+        n="Orange";
+        System.out.println(n+" "+t);
+    }
+
+}
